a87a3ca3e5d2a295d8fcf847babd519373abe9da
jlyons014 <jlyons014@gmail.com>
2019-10-31 10:32:17 -0400

    Added code to Main.Storyboard and ViewController to: add, subtract, multiply, and divide, two numbers and display the result in a label

--- CalculatorApp/ViewController.swift
+++ CalculatorApp/ViewController.swift
@@ -10,11 +10,121 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var firstNumberTextField: UITextField!
+    
+    @IBOutlet weak var secondNumberTextField: UITextField!
+    
+    @IBOutlet weak var resultLabel: UILabel!
+    
+    var result = ""
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func calculateAddition(_ sender: UIButton) {
+        
+        var dFirstNumber = 0.0
+        var dSecondNumber = 0.0
+        var dResult = 0.0
+        
+        if let firstNumber = firstNumberTextField.text, let secondNumber = secondNumberTextField.text {
+            
+            let trimmedFirstNumber = firstNumber.trimmingCharacters(in: .whitespaces)
+            let trimmedSecondNumber = secondNumber.trimmingCharacters(in: .whitespaces)
+            
+            if(!trimmedFirstNumber.isEmpty && !trimmedSecondNumber.isEmpty){
+                
+                dFirstNumber = Double(trimmedFirstNumber)!
+                dSecondNumber = Double(trimmedSecondNumber)!
+                
+                dResult = dFirstNumber + dSecondNumber
+                
+            }
+        }
+        
+        resultLabel.text = "\(String(dResult))"
+   
+        result = "\(String(dResult))"
+        
+    }
+    
+    
+    @IBAction func calculateSubtraction(_ sender: UIButton) {
+        
+        var dFirstNumber = 0.0
+        var dSecondNumber = 0.0
+        var dResult = 0.0
+        
+        if let firstNumber = firstNumberTextField.text, let secondNumber = secondNumberTextField.text {
+            
+            let trimmedFirstNumber = firstNumber.trimmingCharacters(in: .whitespaces)
+            let trimmedSecondNumber = secondNumber.trimmingCharacters(in: .whitespaces)
+            
+            if(!trimmedFirstNumber.isEmpty && !trimmedSecondNumber.isEmpty){
+                
+                dFirstNumber = Double(trimmedFirstNumber)!
+                dSecondNumber = Double(trimmedSecondNumber)!
+                
+                dResult = dFirstNumber - dSecondNumber
+                
+            }
+        }
+        
+        resultLabel.text = " \(String(dResult))"
+    }
+    
+    
+    @IBAction func calculateMultiplication(_ sender: UIButton) {
+        
+        var dFirstNumber = 0.0
+        var dSecondNumber = 0.0
+        var dResult = 0.0
+        
+        if let firstNumber = firstNumberTextField.text, let secondNumber = secondNumberTextField.text {
+            
+            let trimmedFirstNumber = firstNumber.trimmingCharacters(in: .whitespaces)
+            let trimmedSecondNumber = secondNumber.trimmingCharacters(in: .whitespaces)
+            
+            if(!trimmedFirstNumber.isEmpty && !trimmedSecondNumber.isEmpty){
+                
+                dFirstNumber = Double(trimmedFirstNumber)!
+                dSecondNumber = Double(trimmedSecondNumber)!
+                
+                dResult = dFirstNumber * dSecondNumber
+                
+            }
+        }
+        
+        resultLabel.text = " \(String(dResult))"
+    }
+    
+    
+    @IBAction func calculateDivision(_ sender: UIButton) {
+        
+        var dFirstNumber = 0.0
+        var dSecondNumber = 0.0
+        var dResult = 0.0
+        
+        if let firstNumber = firstNumberTextField.text, let secondNumber = secondNumberTextField.text {
+            
+            let trimmedFirstNumber = firstNumber.trimmingCharacters(in: .whitespaces)
+            let trimmedSecondNumber = secondNumber.trimmingCharacters(in: .whitespaces)
+            
+            if(!trimmedFirstNumber.isEmpty && !trimmedSecondNumber.isEmpty){
+                
+                dFirstNumber = Double(trimmedFirstNumber)!
+                dSecondNumber = Double(trimmedSecondNumber)!
+                
+                dResult = dFirstNumber / dSecondNumber
+                
+            }
+        }
+        
+        resultLabel.text = " \(String(dResult))"
+    }
+    
+    
 }
-
